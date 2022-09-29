fbac581f9383790e790d7be6812037b63f5dc7a1
ppenguin <hieronymusv@gmail.com>
2022-09-29 13:10:09 +0200

    remove yaml2 deps

--- viper.go
+++ viper.go
@@ -2133,6 +2133,28 @@ func (v *Viper) getConfigFile() (string, error) {
 	return v.configFile, nil
 }
 
+// Search all configPaths for any config file.
+// Returns the first path that exists (and is a config file).
+func (v *Viper) findConfigFile() (string, error) {
+	finder := finder{
+		paths:            v.configPaths,
+		fileNames:        []string{v.configName},
+		extensions:       SupportedExts,
+		withoutExtension: v.configType != "",
+	}
+
+	file, err := finder.Find(afero.NewIOFS(v.fs))
+	if err != nil {
+		return "", err
+	}
+
+	if file == "" {
+		return "", ConfigFileNotFoundError{v.configName, fmt.Sprintf("%s", v.configPaths)}
+	}
+
+	return file, nil
+}
+
 // Debug prints all configuration registries for debugging
 // purposes.
 func Debug()              { v.Debug() }
